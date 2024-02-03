1b4e4759753844a68956d3091c9536d6b7ecd47a
David Liang <34115092+davidvliang@users.noreply.github.com>
2024-02-03 10:32:45 -1000

    add appbar buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
-import * as React from 'react';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import RefreshIcon from '@mui/icons-material/Refresh';
 
-function App() {
+import AntennaPattern from './components/AntennaPattern';
+import CellArray from './components/CellArray'
+
+export default function App() {
+  const [selectedTab, setSelectedTab] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
   const [count, setCount] = useState(0)
-
   const [status, setStatus] = useState("")
+
+  const submitRef = useRef()
 
   const newStatus = {
     "data": count + 1,
@@ -36,37 +51,54 @@ function App() {
     console.log("")
   }
 
+  const handleStartButton = (event) => {
+    console.log("start button pressed.")
+    setIsRunning(true)
+    submitRef.current.requestSubmit()
+  }
+
+  const handleStopButton = () => {
+    console.log("stop button pressed.")
+    setIsRunning(false)
+  }
+
+  const handleResetButton = () => {
+    console.log("reset button pressed.")
+  }
+
   return (
     <>
-      Here
-      {status}
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained" onClick={handleClick}>Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="static" style={{ position: "sticky", position: "-webkit-sticky" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Avatar variant="rounded" src="public/uhm-coe.png" sx={{ mx: 2 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: "bold" }} display={{ xs: "none", sm: "block" }}>
+            IRS Control System
+          </Typography>
+          <Stack spacing={2} direction="row" sx={{ justifyContent: "end" }}>
+            <Button color="error" variant="contained" onClick={handleStopButton} disabled={!isRunning}>Stop Program</Button>
+            <Button color="success" variant="contained" onClick={handleStartButton} disabled={isRunning}>Start</Button>
+            <Button color="grey" variant="contained" onClick={handleResetButton} disabled={isRunning}><RefreshIcon /></Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      {/* <Tabs value={selectedTab} onChange={(e) => setSelectedTab(e.target.value)}>
+        <Tab label="Main" />
+        <Tab label="Cell Configuration" />
+        <Tab label="Settings" />
+      </Tabs> */}
+
+      <Container maxWidth="lg" style={{ width: "100%", my: 5 }}>
+
+
+
+
+        <Stack spacing={2} direction="row" sx={{ m: 2 }}>
+          <Button variant="contained" onClick={handleClick}>Contained {status}</Button>
+        </Stack>
+        {/* <AntennaPattern /> */}
+        <CellArray submitRef={submitRef} />
+      </Container>
     </>
   )
 }
 
-export default App
