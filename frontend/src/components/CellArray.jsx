1c7dc8dfedff9d14126621a71b1c269573aafc53
David Liang <34115092+davidvliang@users.noreply.github.com>
2024-02-03 10:32:25 -1000

    Create CellArray.jsx

---
+++ frontend/src/components/CellArray.jsx
@@ -0,0 +1,23 @@
+
+
+import { useState, useEffect } from 'react'
+// import { useForm } from 'react-hook-form'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
+
+export default function CellArray({ submitRef }) {
+    // const { register, setValue, getValues, ...other } = useForm()
+
+    return (
+        // <div>
+
+        //     {/* <Button ref={submitRef} type="submit" variant="contained" style={{ display: 'none' }} /> */}
+        // </div>
+        <form name="cellArray" ref={submitRef}>
+
+        </form>
+    )
+}
+
