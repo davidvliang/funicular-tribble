5ff2ff09a4d81e24a5628bb6b2c01a764f156113
David Liang <34115092+davidvliang@users.noreply.github.com>
2024-02-03 10:31:02 -1000

    add themeprovider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createTheme, ThemeProvider, styled, useTheme } from '@mui/material/styles'
+import { grey, green, orange } from '@mui/material/colors';
+
+const themeOptions = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#fafafa',
+    },
+    secondary: {
+      main: '#37856d',
+    },
+    grey: {
+      main: grey[600],
+      dark: grey[900],
+      contrastText: '#fff',
+    }
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeOptions}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
