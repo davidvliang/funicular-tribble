5cf945dff0eb3c811f28aae69b923f796eb4dee2
David Liang <34115092+davidvliang@users.noreply.github.com>
2024-02-04 18:17:09 -1000

    init antenna pattern

--- frontend/src/components/AntennaPattern.jsx
+++ frontend/src/components/AntennaPattern.jsx
@@ -1,21 +1,17 @@
-import { useState, useEffect, useRef } from 'react'
-import { LineChart } from '@mui/x-charts/LineChart';
+import { useState, useEffect, useRef } from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function AntennaPattern() {
-
-
-    return (
-        <LineChart
-            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-            series={[
-                {
-                    data: [2, 5.5, 2, 8.5, 1.5, 5],
-                },
-            ]}
-            width={500}
-            height={300}
-            
-        />
-    );
-
+  return (
+    <LineChart
+      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      series={[
+        {
+          data: [2, 5.5, 2, 8.5, 1.5, 5],
+        },
+      ]}
+      // width={500}
+      height={200}
+    />
+  );
 }
