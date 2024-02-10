796801b61ad4446fcba60c4f6e1cc5cb94e291d7
David Liang <34115092+davidvliang@users.noreply.github.com>
2024-02-09 16:44:40 -1000

    init RHF in auto-configuration

--- frontend/src/components/AutoConfiguration.jsx
+++ frontend/src/components/AutoConfiguration.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { useFormContext } from "react-hook-form";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 
@@ -13,34 +14,36 @@ import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 import DeviationSlider from "./DeviationSlider";
 
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
 
-export default function AutoConfiguration({ isRunning }) {
-  const [value, setValue] = useState([0, -10]);
+export default function AutoConfiguration(props) {
+  const {
+    isRunning,
+    isReset,
+    ...other
+  } = props;
+
+  const { register, setValue, getValues, reset } = useFormContext();
+
+  const [angle, setAngle] = useState([0, 0]);
+
+  useEffect(() => {
+    console.log("RESET BUTTON AUTO", angle)
+    setAngle([0, 0])
+  }, [isReset])
+
 
   const handleSliderChange = (event, newValue) => {
-    setValue(newValue);
+    setAngle(newValue);
   };
 
   const handleInputChange = (event, newValue) => {
-    // setValue(event.target.value === "" ? 0 : Number(event.target.value));
-    setValue(newValue);
+    setAngle(newValue);
   };
 
   const handleBlur = () => {
-    if (value < 0) {
+    if (angle < 0) {
       setValue(0);
-    } else if (value > 100) {
+    } else if (angle > 100) {
       setValue(100);
     }
   };
@@ -57,7 +60,7 @@ export default function AutoConfiguration({ isRunning }) {
         sx={{ my: 2 }}
       >
         <TextField
-          value={value[0] + value[1]}
+          value={angle[0] + angle[1]}
           size="small"
           // type="number"
           onChange={handleInputChange}
@@ -70,20 +73,20 @@ export default function AutoConfiguration({ isRunning }) {
         <Grid item xs={12}>
           <Slider
             // value={typeof value === "number" ? value : 0}
-            value={value}
+            value={angle}
             defaultValue={[0, 10]}
             min={-45}
             max={45}
             step={5}
             onChange={handleSliderChange}
             disabled={isRunning}
-            // track={false}
-            // marks
-            // sx={{
-            //   "& .MuiSlider-thumb": {
-            //     display: "none",
-            //   },
-            // }}
+          // track={false}
+          // marks
+          // sx={{
+          //   "& .MuiSlider-thumb": {
+          //     display: "none",
+          //   },
+          // }}
           />
           {/* <DeviationSlider /> */}
         </Grid>
